2b6dcf6bba14ca0dd70e9ca5811f612be45108b2
steven <sseller6@byui.edu>
2022-04-07 21:44:02 -0600

    did prove_07

---
+++ CSE110_review/prove_07.py
@@ -0,0 +1,13 @@
+# number = int(input('Please type a positive number: '))
+
+# while number <= 0:
+#     number = int(input('Sorry, that is a negative number. Please try again. '))
+
+# print(f'The number is {number}')
+
+answer = input('May I have a piece of candy? ')
+
+while answer != 'yes':
+    answer = input('May I have a piece of candy? ')
+
+print('Thank you.')
