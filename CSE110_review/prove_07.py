a64e90b63795a86ec13d76fc091b3c87d48b302f
steven <sseller6@byui.edu>
2022-04-10 15:14:20 -0600

    half way through prove_07

--- CSE110_review/prove_07.py
+++ CSE110_review/prove_07.py
@@ -1,3 +1,6 @@
+"""
+Practice while loops
+"""
 # number = int(input('Please type a positive number: '))
 
 # while number <= 0:
@@ -5,9 +8,62 @@
 
 # print(f'The number is {number}')
 
-answer = input('May I have a piece of candy? ')
+# answer = input('May I have a piece of candy? ')
 
-while answer != 'yes':
-    answer = input('May I have a piece of candy? ')
+# while answer != 'yes':
+#     answer = input('May I have a piece of candy? ')
 
-print('Thank you.')
+# print('Thank you.')
+"""
+Practice for loops
+"""
+# colors = ["red", "blue", "green", "yellow"]
+# for color in colors:
+#     print(color)
+
+# number_list = range(1,9)
+# for number in number_list:
+#     print(number)
+
+# even_number_list = range(2,20,2)
+# for even_number in even_number_list:
+#     print(even_number)
+
+"""
+Assignment V
+"""
+# Start of Game
+import random
+print()
+print('Welcome to the word guessing game!')
+
+list_of_secret_words = ['table','mouse','money']
+
+secret_word = random .choice(list_of_secret_words)
+print('Your hint is:_ _ _ _ _')
+user_guess =input('What is your guess? ')
+
+guess_count = 1
+
+# Game Logic
+while user_guess != secret_word:
+    for secret_letters in secret_word:
+        for guess_letters in user_guess:
+            if secret_letters == guess_letters:
+                guess_letters = guess_letters.upper()
+            elif secret_letters != guess_letters:
+                guess_letters ='_'
+
+    print('Sorry, that was incorrect.')
+    user_guess = input('What is your guess? ')
+    guess_count += 1
+        
+
+# End of Game
+if user_guess == secret_word:
+    print('Congratulations! You guessed it!')
+
+    if guess_count == 1:
+        print(f'It took you {guess_count} guess.')
+    elif guess_count > 1:
+        print(f'It took you {guess_count} guesses.')
