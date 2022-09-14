fe61a4df98d53083b55d329806d59ee320d917db
steven <sseller6@byui.edu>
2022-09-13 21:18:51 -0600

    I'm never forgetting

---
+++ CSE210_Programming_With_Classes/week_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("This is in C#!");
+
+            // int number = 5;
+            // number = 8;
+            // number = number +3;
+
+            // string color = "blue";
+
+            // Console.WriteLine("What is your favorite color? ");
+            // string user_color = Console.ReadLine();
+            // Console.WriteLine($"Your favorite color is {user_color}, and mine is {color}");
+
+            //Beginning of first assignment.
+
+            
+        }
+    }
+}
