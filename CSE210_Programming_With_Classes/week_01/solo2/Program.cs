063262f1406b8ef437ac89fcbd3876d4d41de452
steven <sseller6@byui.edu>
2022-09-15 18:52:26 -0600

    finished solo prep 3

---
+++ CSE210_Programming_With_Classes/week_01/solo2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Assignment
+            Portion*/
+
+            string letter = "";
+            
+            Console.Write("What is your grade percentage? ");
+            string userGradePercentage = Console.ReadLine();
+
+            int x = int.Parse(userGradePercentage);
+
+            Console.WriteLine($"Your grade percentage is: % {userGradePercentage}.");
+
+            if (x >= 90)
+            {
+                letter = "A";
+                Console.WriteLine($"Your letter grade for this course is an {letter}.");
+            }
+            else if (x >= 80)
+            {
+                letter = "B";                
+                Console.WriteLine($"Your letter grade for this course is a {letter}.");
+            }
+            else if (x >= 70)
+            {
+                letter = "C";
+                Console.WriteLine($"Your letter grade for this course is a {letter}.");
+            }
+            else if (x >= 60)
+            {
+                letter = "D";
+                Console.WriteLine($"Your letter grade for this course is a {letter}.");
+            }
+            else if (x < 60)
+            {
+                letter = "F";
+                Console.WriteLine($"Your letter grade for this course is a {letter}.");
+            }
+            else{
+                Console.WriteLine($"Please re-enter your grade percentage.");
+            }
+
+            if (x >= 70)
+            {
+                Console.WriteLine("Congrats! You passed the course.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you did not pass the course.");
+            }
+            /* Guided
+            Portion */
+
+            // Console.Write("Enter Number for X: ");
+            // string valueFromUser = Console.ReadLine();
+
+            // Console.Write("Enter Number for Y: ");
+            // string secondValueFromUser = Console.ReadLine();
+            
+            // int x = int.Parse(valueFromUser);
+            // int y = int.Parse(secondValueFromUser);
+
+            // if (x > y)
+            // {
+            //     Console.WriteLine("Greater");
+            // }
+            // else if (x < y)
+            // {
+            //     Console.WriteLine("Less");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Not Greater");
+            // }
+
+
+            /* Individual Practice*/
+
+
+            // if (x > y)
+            // {
+            //     Console.WriteLine("greater than y");
+            // }
+            // else if (x > z)
+            // {
+            //     Console.WriteLine("greater than z");
+            // }
+            // else 
+            // {
+            //     Console.WriteLine("less than both");
+            // }
+
+            // if (name =="John")
+            // {
+            //     Console.WriteLine("The name is John");
+            // }
+            // if (Color != favorite_color)
+            // {
+            //     Console.WriteLine("That color is not my favorite");
+            // }
+            // and is && or is || not is !
+            
+        }
+    }
+}
+
+// Throw this into the console at the start of each project. "dotnet new console --framework net6.0"
---
+++ CSE210_Programming_With_Classes/week_01/solo3/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("What is the magic number? ");
+            // string userNumberChoice = Console.ReadLine();
+
+            Random randomGenerator = new Random();
+
+            int magic_number = randomGenerator.Next(1,10);
+
+            int userGuessNumber = 0;
+
+            while (userGuessNumber != magic_number)
+            {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            userGuessNumber = int.Parse(userGuess);
+
+            if (userGuessNumber > magic_number)
+            {
+                Console.WriteLine("Your guess is higher. ");
+            }
+
+            else if (userGuessNumber < magic_number)
+            {
+                Console.WriteLine("Your guess is lower. ");
+            }
+
+            else if (userGuessNumber == magic_number)
+            {
+                Console.WriteLine("You guessed it! ");
+            }
+
+            else
+            {
+                Console.WriteLine("Your guess was not formatted correctly. ");
+            }
+
+            }
+            /* Solo Practice */
+
+            // string input ="yes";
+
+            // while (input == "yes")
+            // {
+            //     Console.Write("Do you want to continue? ");
+            //     input = Console.ReadLine();
+            // }
+
+            // do
+            // {
+            //     Console.Write("Do you want to continue? ");
+            //     input = Console.ReadLine();
+            // } while (input == "yes");
+
+            // // This for loops counts from 0 to 9.
+            // for (int i = 0; i < 10; i++)
+            // /* The first part initializes the value, the second is the condition to check, 
+            // and the third is the increment step taht is run at the end of each loop. */
+            // // the ++ operator increments the value in the variable by one.
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // for (int i = 2; i <=20; i = i + 2)
+            // /* Same as previous for loop except it counts by twos.*/
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // foreach (string color in colors)
+            // {
+            //     Console.WriteLine(color);
+            // }
+
+            // Random randomGenerator = new Random();
+            // int number = randomGenerator.Next(1,11);
+
+        }
+    }
+}
