2badf2356393190a83932c4d24144c35ca0e6726
steven <sseller6@byui.edu>
2023-05-18 13:09:41 -0600

    First step to cleaning up repo.

--- CSE110_review/prove_01.py
+++
@@ -1,2 +0,0 @@
-favorite_color = input('Please type your favorite color: ')
-print(f'Your favorite color is {favorite_color}')
--- CSE121_b_JavaScript/Week 2/debugging_practice/debugging.js
+++
@@ -1,8 +0,0 @@
-const PI = 3.14;
-let radius = 3;
-let area = 0;
-area = radius * radius * PI;
-console.log("Area1:", area);
-radius = 4;
-area = radius * radius * PI;
-console.log("Area2:", area);
